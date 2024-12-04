92226f1ad64af15629983d001695ac87fd366ea8
Matuotyan <Kuniteru.123@me.com>
2024-12-04 18:18:49 +0900

    header 作成

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,27 @@
+import"./App.scss"
 export default function Home() {
   return (
-    <div>
-      
+    <div className="all_logo_frame">
+      <a className="logo_flex">
+        <div className="img"></div>
+        <div>
+          <p className="logo_title">billbox</p>
+        </div>
+      </a>
+      <div className="contents">
+        <a className="content">特徴</a>
+        <a className="content">料金プラン</a>
+        <a className="content">Pricing</a>
+        <a className="content">About</a>
+      </div>
+      <div className="login_sinki">
+        <div className="login_frame">
+          <a className="login">ログイン</a>
+        </div>
+        <div className="sinki_frame">
+          <a className="sinki">新規登録</a>
+        </div>
+      </div>
     </div>
   );
 }
